04371fe79e3fb9f63766d3381da5f9379b670ccb
j-segura <juanda.segura2005@gmail.com>
2023-05-31 23:40:11 -0500

    starting with Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeAppComponent } from './components/home-app/home-app.component';
+import { AboutAppComponent } from './components/about-app/about-app.component';
+import { ContactAppComponent } from './components/contact-app/contact-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home-app',
+  },
+  { path: 'home-app', component: HomeAppComponent, },
+  { path: 'about-app', component: AboutAppComponent, },
+  { path: 'contact-app', component: ContactAppComponent, },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'home-app',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeAppComponent } from './components/home-app/home-app.component';
+import { AboutAppComponent } from './components/about-app/about-app.component';
+import { ContactAppComponent } from './components/contact-app/contact-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeAppComponent,
+    AboutAppComponent,
+    ContactAppComponent
   ],
   imports: [
     BrowserModule,
